7dd76f294853bb75e9672f18b4547e91e1d71728
arman <united.abominations95@gmail.com>
2017-03-11 15:45:44 -0600

    php login

--- user/login2.php
+++ user/login2.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header("Access-Control-Allow-Origin: {$_SERVER['http://198.199.101.234']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');   
     }
@@ -13,7 +13,7 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
  
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            header("Access-Control-Allow-Headers:        *)";
  
         exit(0);
     }
