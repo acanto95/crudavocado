0156013937f1ae708abde385bfe6b7b9c1351093
arman <united.abominations95@gmail.com>
2017-03-11 12:53:23 -0600

    crud evento con database php

---
+++ event/database.php
@@ -0,0 +1,31 @@
+<?php
+	class Database {
+		private static $dbName 			= 'avocado' ; 
+		private static $dbHost 			= 'localhost' ;
+		private static $dbUsername 		= 'admin';
+		private static $dbUserPassword 	= '56add18d8204bdf3e6d9bdb12fa2f9a5f1e3942be3adab53';
+		
+		private static $cont  = null;
+		
+		public function __construct() {
+			exit('Init function is not allowed');
+		}
+		
+		public static function connect(){
+		   // One connection through whole application
+	    	if ( null == self::$cont ) {      
+		    	try {
+		        	self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+		        }
+		        catch(PDOException $e) {
+		        	die($e->getMessage());  
+		        }
+	       	} 
+	       	return self::$cont;
+		}
+		
+		public static function disconnect() {
+			self::$cont = null;
+		}
+	}
+?>
